b145a6e60531d6c8e734345f4191e1da90e23925
kiieeaa <kiyaadinda5@gmail.com>
2025-11-05 11:43:27 +0700

    menambahkan app post

--- index.js
+++ index.js
@@ -16,8 +16,7 @@ db.sequelize.sync()
     .catch((err) => {
         console.log(err);
     })
-
-    app.post('/hotel', async (req, res) => {
+     app.post('/hotel', async (req, res) => {
         const data = req.body;
         try{
             const hotel = await db.Tentrem.create(data);
@@ -28,44 +27,5 @@ db.sequelize.sync()
         // 'error' tidak dikenal (not defined) di sini
         res.send({message: error.message}); 
     });
-
-     app.get('/hotel', async (req, res) => {
-        try{
-            const hotel = await db.Tentrem.findAll();
-            res.send(hotel);
-        }
-        catch (error){
-            res.send({message: error.message});
-        }
-    });
-     app.put('/hotel/:id', async (req, res) => {
-        const id = req.params.id;
-        const data = req.body;
-        try{
-            const hotel = await db.Tentrem.findByPk(id);
-            if (!hotel) {
-                return res.status(404).send({message: 'hotel not found'});
-            }
-            await hotel.update(data);
-            res.send({message: "hotel berhasil di update", hotel});
-        }
-        catch (error){
-            res.send({message: error.message});
-        }
-    });
-
-      app.delete('/hotel/:id', async (req, res) => {
-        const id = req.params.id;
-        try{
-            const hotel = await db.Tentrem.findByPk(id);
-            if(!hotel){
-                return res.status(404).send({message: 'hotel not found'});
-            }
-            await hotel.destroy();
-            res.send({message: 'belajar berhasil dihapus'});
-        }catch (error){
-            res.status(500).send(error.message);
-        }
-    });
 
 
